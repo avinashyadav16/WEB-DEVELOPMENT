43b3ec3030c5f885a225dec8b6ca3b755042f7d3
Avinash Yadav <avinashurmilayadav@gmail.com>
2024-02-22 01:07:01 +0530

    Added NodeJS

---
+++ CHAPTER-06 NODEJS/index.js
@@ -0,0 +1,15 @@
+//jshint esversion:6
+
+
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperHeroName = superheroes.random();
+var mySuperVillainName = superheroes.random();
+
+console.log("Super Hero is: " + mySuperHeroName);
+console.log("Super Villain is: " + mySuperVillainName);
+
+
+
+
