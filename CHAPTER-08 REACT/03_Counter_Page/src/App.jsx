95435b3b7899ccc7cc81c013d8a05a097310dcfe
Avinash Yadav <avinashurmilayadav@gmail.com>
2024-03-12 15:59:03 +0530

    Counter Page Init

---
+++ CHAPTER-08 REACT/03_Counter_Page/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, setNewCounter] = useState(0);
+
+  const increaseValue = () => {
+    setNewCounter(counter + 1);
+
+    /*
+    setNewCounter(prevCounter => prevCounter + 1);
+    setNewCounter(prevCounter => prevCounter + 1);
+    setNewCounter(prevCounter => prevCounter + 1);
+    setNewCounter(prevCounter => prevCounter + 1);
+    */
+  };
+
+  const decreaseValue = () => {
+    if (counter === 0) {
+      setNewCounter(0);
+    } else {
+      setNewCounter(counter - 1);
+    }
+  };
+
+  const resetValue = () => {
+    setNewCounter(0);
+  };
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h3>Counter Value: {counter}</h3>
+      <br />
+      <br />
+      <button onClick={increaseValue}> Increase Value </button>
+      <button onClick={decreaseValue}> Decrease Value </button>
+
+      <br />
+      <button onClick={resetValue}> Reset Counter </button>
+    </>
+  );
+}
+
+export default App;
