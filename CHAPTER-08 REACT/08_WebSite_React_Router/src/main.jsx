992ce95ef272d59e826aadd9d5b848eeb48ce920
Avinash Yadav <avinashurmilayadav@gmail.com>
2024-03-13 23:47:25 +0530

    Router SetUps

---
+++ CHAPTER-08 REACT/08_WebSite_React_Router/src/main.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import ContactUs from "./components/ContactUs/ContactUs";
+import Route from "./Route";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Route />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contactus",
+        element: <ContactUs />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
